37cc1cff44166efd0c0b953254658f21df1a7314
VKiran <vkiran@whiteclarkegroup.com>
2020-04-24 10:44:21 +0100

    Added Swagger integration Added Exception Handler Framework

--- define/src/main/java/com/ecomm/define/controller/BigCommerceProductCSVController.java
+++ define/src/main/java/com/ecomm/define/controller/BigCommerceProductCSVController.java
@@ -1,18 +1,24 @@
 package com.ecomm.define.controller;
 
 import com.ecomm.define.domain.BigCommerceProduct;
-import com.ecomm.define.repository.BigCommerceProductRepository;
 import com.ecomm.define.service.BigCommerceService;
 import com.ecomm.define.service.ValidateCSVService;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -23,7 +29,8 @@ import java.util.List;
  * Created by vamshikirangullapelly on 18/04/2020.
  */
 @RestController
-@RequestMapping("/bcproduct")
+@RequestMapping("/define")
+@Api(value="BigCommerceCSVGenerator", description="Operation to generate Big Commerce CSV")
 public class BigCommerceProductCSVController {
 
     @Autowired
@@ -34,8 +41,16 @@ public class BigCommerceProductCSVController {
     private static final String BIG_COMMERCE_CSV = "big-commerce.csv";
 
 
-    @GetMapping("/generate-csv-file")
-    public String uploadCSVFile( Model model) {
+    @ApiOperation(value = "Generate CSV File",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully generates csv"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @GetMapping("/bcproduct/generate-csv-file")
+    public String generateBigCommerceCSVFile() throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
 
         try (Writer writer = Files.newBufferedWriter(Paths.get(PATH+BIG_COMMERCE_CSV),StandardCharsets.UTF_8)) {
                 StatefulBeanToCsv<BigCommerceProduct> beanToCsv = new StatefulBeanToCsvBuilder(writer)
@@ -51,9 +66,9 @@ public class BigCommerceProductCSVController {
 
             beanToCsv.write(commerceProductsList);
 
-            } catch (Exception ex) {
+            } /*catch (Exception ex) {
                 model.addAttribute("status", false);
-            }
-        return "file-7770-status";
+            }*/
+        return "Successfully Generated CSV File";
     }
 }
--- define/src/main/java/com/ecomm/define/controller/MaisonProductUploadController.java
+++ define/src/main/java/com/ecomm/define/controller/MaisonProductUploadController.java
@@ -1,15 +1,23 @@
 package com.ecomm.define.controller;
 
 import com.ecomm.define.domain.MaisonProduct;
+import com.ecomm.define.exception.CustomExceptionHandler;
+import com.ecomm.define.exception.RecordNotFoundException;
 import com.ecomm.define.service.GenerateBCDataService;
 import com.ecomm.define.service.MaisonService;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
@@ -27,7 +36,8 @@ import java.util.List;
  * Created by vamshikirangullapelly on 18/04/2020.
  */
 @RestController
-@RequestMapping("/maison")
+@RequestMapping("/define")
+@Api(value="MaisonProductCSVUpload", description="Uploads Maison products and saves in the DB")
 public class MaisonProductUploadController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MaisonProductUploadController.class);
@@ -38,18 +48,47 @@ public class MaisonProductUploadController {
     @Autowired
     private GenerateBCDataService generateBCDataService;
 
-    @GetMapping(value = "/")
+    @ApiOperation(value = "Retrieves All Maison Products From DB",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved all Maison products"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @GetMapping(value = "/maison")
     public List<MaisonProduct> getAllProducts() {
         return maisonService.findAll();
     }
 
-    @GetMapping(value = "/{id}")
-    public MaisonProduct getProductById(@PathVariable("id") ObjectId id) {
-        return maisonService.findBy_Id(id);
+    @ApiOperation(value = "Retrieves Maison Product by ID From DB",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved Maison product by ID from DB"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @GetMapping(value = "/maison/{id}")
+    public ResponseEntity<MaisonProduct> getProductById(@PathVariable("id") ObjectId id) {
+        MaisonProduct maisonProduct = maisonService.findBy_Id(id);
+        if(maisonProduct == null) {
+            throw new RecordNotFoundException("Invalid product id : " + id);
+        }
+        return ResponseEntity.ok().body(maisonProduct);
     }
 
-    @PutMapping("/upload-csv-file")
-    public String uploadCSVFile(@RequestParam("file") MultipartFile file, Model model) {
+
+    @ApiOperation(value = "Uploads CSV File From Maison to DB",response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully uploaded Maison Products to DB"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @PutMapping("/maison/upload-csv-file")
+    public ResponseEntity<String> uploadCSVFile(@RequestParam("file") MultipartFile file, Model model) throws IOException {
 
         LOGGER.info("started uploading from file - {}", file.getOriginalFilename());
 
@@ -85,12 +124,12 @@ public class MaisonProductUploadController {
                 model.addAttribute("status", true);
                 generateBCDataService.generateBcData();
 
-            } catch (Exception ex) {
+            }/* catch (Exception ex) {
                 model.addAttribute("message", "An error occurred while processing the CSV file.");
                 model.addAttribute("status", false);
-            }
+            }*/
         }
 
-        return "file-upload-status";
+        return ResponseEntity.ok().body("Successfully uploaded");
     }
 }
